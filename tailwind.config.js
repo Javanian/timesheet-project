92e84eb59a582892f8f8ee0800593f084da2592c
Javanian <teesanfajar@gmail.com>
2025-11-21 11:53:45 +0700

    new UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+        "./UI/*.html",
+    "./UI/**/*.html",
+    "./js/*.js",
+    "./js/**/*.js",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
